8127c8459a24e4f9dfd50f5575f1b1db85fa00d9
Simon Bergot <sbergot@cegid.com>
2024-09-23 13:19:45 +0200

    review abilities

---
+++ tools/format.mjs
@@ -0,0 +1,15 @@
+import { apply } from "./io.mjs"
+
+[
+  "abilities",
+  "ability-trees",
+  "chassisData",
+  "classes",
+  "equipment",
+  "modules",
+  "otherEntities",
+  "systems"
+].forEach(name => {
+  const filename = `data/${name}.json`;
+  apply(filename, d => d);
+})
