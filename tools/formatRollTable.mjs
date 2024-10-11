7b42ea70e1c62b21d539e2186a5d21f7aa240feb
Simon Bergot <sbergot@cegid.com>
2024-10-11 10:42:06 +0200

    add tool to format roll table

---
+++ tools/formatRollTable.mjs
@@ -0,0 +1,13 @@
+import * as fs from "fs"
+
+
+const content = fs.readFileSync("temp.txt").toString();
+
+let result = content;
+result = result.replace(/[\r][\n](\D)/g, " $1");
+result = result.replace(/(\d+) - (\d+)/g, "$1-$2");
+result = result.replace(/(.+): (.+)/g, '"$1": "$2",');
+result = result.replace(/,$/g, '');
+result = ', "rollTable": {' + result + "}"
+
+fs.writeFileSync("result.txt", result);
