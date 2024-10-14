a63420971eafcacb96df1db5915085f29e2c7d8e
Simon Bergot <sbergot@cegid.com>
2024-10-14 13:00:12 +0200

    add abilities rolltables

--- tools/formatRollTable.mjs
+++ tools/formatRollTable.mjs
@@ -6,7 +6,8 @@ const content = fs.readFileSync("temp.txt").toString();
 let result = content;
 result = result.replace(/[\r][\n](\D)/g, " $1");
 result = result.replace(/(\d+) - (\d+)/g, "$1-$2");
-result = result.replace(/(.+): (.+)/g, '"$1": "$2",');
+result = result.replace(/(\d.*?): (.+)/g, '"$1": "$2",');
+result = result.replace(/- /g, '');
 result = result.replace(/,$/g, '');
 result = ', "rollTable": {' + result + "}"
 
